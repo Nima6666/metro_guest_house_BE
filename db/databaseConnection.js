e02da0ab007b960a21f0bb61a7e8c97217e7a214
nima <lamap8708@gmail.com>
2024-04-23 17:14:32 +0545

    user fetching

--- db/databaseConnection.js
+++ db/databaseConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGOURL)
-  .then(console.log("connected to database"));
+    .connect(process.env.MONGOURL)
+    .then(console.log("connected to database"));
--- routes/users.js
+++ routes/users.js
@@ -7,53 +7,61 @@ const multer = require("multer");
 const { isAuthenticated } = require("../middleware/userAuth");
 
 const storage = multer.diskStorage({
-  destination: function (req, image, cb) {
-    cb(null, "uploads/profile/");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.originalname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+    destination: function (req, image, cb) {
+        cb(null, "uploads/profile/");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.originalname +
+                "-" +
+                Date.now() +
+                path.extname(file.originalname)
+        );
+    },
 });
 
 const uploadProfile = multer({
-  storage: storage,
+    storage: storage,
 });
 
 const storageDocument = multer.diskStorage({
-  destination: function (req, image, cb) {
-    cb(null, "uploads/document/");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.originalname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+    destination: function (req, image, cb) {
+        cb(null, "uploads/document/");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.originalname +
+                "-" +
+                Date.now() +
+                path.extname(file.originalname)
+        );
+    },
 });
 
 const uploadDocument = multer({
-  storage: storageDocument,
+    storage: storageDocument,
 });
 
 router.get("/", isAuthenticated, userController.getUsers);
 
 router.post(
-  "/register",
-  uploadProfile.single("image"),
-  userController.register
+    "/register",
+    uploadProfile.single("image"),
+    userController.register
 );
 router.post("/login", userController.login);
 
 router.get("/getCurrentUser", isAuthenticated, userController.getCurrentUser);
 
 router.post(
-  "/visitor",
-  isAuthenticated,
-  uploadDocument.single("image"),
-  userController.addVisitor
+    "/visitor",
+    isAuthenticated,
+    uploadDocument.single("image"),
+    userController.addVisitor
 );
+
+router.get("/:id", isAuthenticated, userController.getUser);
 
 module.exports = router;
